8fb6465bb54b7aa4e7ba1323f350b538428291df
David Jones <djones6@uk.ibm.com>
2019-06-24 17:04:40 +0100

    Initial commit

---
+++ test.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+// required for statx() system call
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/stat.h>
+#include <stdio.h>
+#define AT_STATX_SYNC_AS_STAT   0x0000  /* - Do whatever stat() does */
+
+// There is no glibc statx() function, it must be called using syscall().
+
+static inline ssize_t
+_statx(int dfd, const char *filename, unsigned int flags, unsigned int mask, struct statx *buffer) {
+    return syscall(__NR_statx, dfd, filename, flags, mask, buffer);
+}
+
+int main () {
+	char *filename = "/bin/ls";
+	struct statx statx_buffer = {0};
+	ssize_t ret = _statx(AT_FDCWD, filename, AT_SYMLINK_NOFOLLOW | AT_STATX_SYNC_AS_STAT, STATX_ALL, &statx_buffer);
+	if (ret == 0) {
+      		printf("_statx succeeded: %lu\n", ret );
+	} else {
+      		printf("_statx failed: %lu\n", ret );
+	}
+   	return(ret);
+}
+
